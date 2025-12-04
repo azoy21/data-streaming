d8eae5cdf878379f531a4bd74a1a741a8f29ad79
azoy21 <76914391+azoy21@users.noreply.github.com>
2025-12-04 14:25:44 +0700

    Update API app.py

--- api/app.py
+++ api/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import psycopg2
 
 app = Flask(__name__)
+CORS(app)  # 👉 IZINKAN semua origin (3000 boleh akses)
 
 def get_data():
     conn = psycopg2.connect(
